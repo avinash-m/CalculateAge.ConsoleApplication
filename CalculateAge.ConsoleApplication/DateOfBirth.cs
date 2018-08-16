79b134b10ff2f93d3e1593783853f0c886fa3041
Avinash M <avinashm201993@gmail.com>
2018-08-16 17:39:57 -0500

    Check the input for valid date

---
+++ CalculateAge.ConsoleApplication/DateOfBirth.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace CalculateAge.ConsoleApplication
+{
+    class DateOfBirth
+    {
+        int day;
+        int month;
+        int year;
+
+        public int Day
+        {
+            get { return day; }
+            set { day = value; }
+        }
+
+        public int Month
+        {
+            get { return month; }
+            set { month = value; }
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public DateOfBirth()
+        {
+            Day = 1;
+            Month = 1;
+            Year = 1900;
+        }
+
+        public bool IsValidDate(string date)
+        {
+            DateTime temp;
+            if (DateTime.TryParse(date, out temp))
+            {
+                this.Day = temp.Day;
+                this.Month = temp.Month;
+                this.Year = temp.Year;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public void ShowAge()
+        {
+
+        }
+    }
+}
